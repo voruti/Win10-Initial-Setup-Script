55fa045d8ee3861f8a126b6cebf0952372110367
voruti <unconfigured>
2019-05-14 21:23:40 +0200

    Add firefox download function

--- myScript.psm1
+++ myScript.psm1
@@ -2,7 +2,10 @@
 
 # Download Firefox
 Function DownloadFirefox {
-	
+	$firefoxUrl = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=de"
+	$firefoxOutput = "$PSScriptRoot\firefox_latest_ssl_win64_de.exe"
+
+	Invoke-WebRequest -Uri $firefoxUrl -OutFile $firefoxOutput
 }
 
 
