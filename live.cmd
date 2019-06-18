d9ef0b46ae2180f89e3fd4d463f427171dbf5299
voruti <unconfigured>
2019-06-18 20:10:20 +0200

    Update live.cmd

--- live.cmd
+++ live.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 
-set /p command=""
+set /p command="Enter tweaks to run them: "
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" RequireAdmin %command% WaitForKey
