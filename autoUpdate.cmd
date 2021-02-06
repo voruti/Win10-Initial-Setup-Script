8d62a2237feecd9d4c619b2a8b52b5e6999a5b59
voruti <45130096+voruti@users.noreply.github.com>
2021-02-06 14:22:09 +0100

    Add git pull to autoUpdate

--- autoUpdate.cmd
+++ autoUpdate.cmd
@@ -1,1 +1,7 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" -preset "%~dpn0.preset"
+@echo off
+
+cd /D "%~dp0"
+git pull
+echo.
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" -preset "%~dpn0.preset"
