1883b87f2a9c97c447536c1776bbea2548979cfe
voruti <45130096+voruti@users.noreply.github.com>
2021-05-12 20:57:36 +0200

    Remove RequireAdmin from live

--- live.cmd
+++ live.cmd
@@ -3,4 +3,4 @@
 
 set /p command="Enter tweaks to run them: "
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" RequireAdmin ClearConsole %command% WaitForKey
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" ClearConsole %command% WaitForKey
