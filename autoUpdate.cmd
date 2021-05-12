f2e6cdc3bb60477d7a7fa8b314e6c3b4c499a080
voruti <45130096+voruti@users.noreply.github.com>
2021-05-12 20:17:31 +0200

    Add ClearConsole

--- autoUpdate.cmd
+++ autoUpdate.cmd
@@ -4,4 +4,4 @@ cd /D "%~dp0"
 git pull
 echo.
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" -preset "%~dpn0.preset"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -include "%~dp0myScript.psm1" ClearConsole -preset "%~dpn0.preset"
